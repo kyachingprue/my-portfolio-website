90d4d8356001716d01c1c9113db0fb6904c7f164
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-06-21 17:53:53 +0600

    final update commit

--- src/components/TextChange.jsx
+++ src/components/TextChange.jsx
@@ -1,41 +1,26 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 const TextChange = () => {
-  const texts = useMemo(() =>
-    ["I'm Kachingprue Marma",
-      "I'm Coder",
-      "I'm Programmer",
-      "I'm Web Developer",
-      "I'm Full-stack Developer",
-      "I'm Software Developer"], []);
-
-  const [currentText, setCurrentText] = useState("");
-  const [endValue, setEndValue] = useState(0);
-  const [isForward, setIsForward] = useState(true);
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentText(texts[index].substring(0, endValue));
-      if (isForward) {
-        setEndValue((prev) => prev + 1);
-      } else {
-        setEndValue((prev) => prev - 1);
-      }
-      if (endValue > texts[index].length + 10) {
-        setIsForward(false)
-      }
-      if (endValue < 2.1) {
-        setIsForward(true)
-        setIndex((prev) => (prev + 1) % texts.length)
-      }
-    }, 50)
-    return () => clearInterval(intervalId);
-  }, [endValue, isForward, index, texts])
+  const [text] = useTypewriter({
+    words: ['Kyachingprue Marma', 'Web Developer', 'Programmer', 'Full-Stack Developer', 'Coder'],
+    loop: Infinity,
+    typeSpeed: 70,
+    deleteSpeed: 50,
+    delaySpeed: 1000
+  })
   return (
-    <div className='transition ease duration-300'>{currentText}
-      <span className="blinking-cursor">|</span>
-    </div>)
+    <div>
+      <h1 className='p-2'>
+        I'm {' '}
+        <span style={{ fontWeight: 'bold', color: 'skyblue' }}>
+          {text}
+        </span>
+        <span style={{ color: 'white' }}>
+          <Cursor />
+        </span>
+      </h1>
+    </div>
+  );
 };
 
 export default TextChange;
